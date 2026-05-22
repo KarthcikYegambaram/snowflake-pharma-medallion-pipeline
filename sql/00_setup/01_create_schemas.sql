76685166c7ad1c48713ac2e3cfb08a4bb92c734e
Karthick Yegambaram <143865296+KarthcikYegambaram@users.noreply.github.com>
2026-05-22 16:43:28 +0530

    Create schemas for KARTHICKY_DB

--- sql/00_setup/01_create_schemas.sql
+++ sql/00_setup/01_create_schemas.sql
@@ -1,1 +1,10 @@
 
+-- ============================================================
+-- CREATE SCHEMAS
+-- ============================================================
+
+CREATE SCHEMA IF NOT EXISTS KARTHICKY_DB."1_BRONZE";
+
+CREATE SCHEMA IF NOT EXISTS KARTHICKY_DB."2_SILVER";
+
+CREATE SCHEMA IF NOT EXISTS KARTHICKY_DB."3_GOLD";
