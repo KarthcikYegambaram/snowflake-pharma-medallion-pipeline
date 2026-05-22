f09d0820b0a38e7c616b3d13197198d9ad5c5470
Karthick Yegambaram <143865296+KarthcikYegambaram@users.noreply.github.com>
2026-05-22 16:46:21 +0530

    Update 02_create_rejected_records.sql

--- sql/02_silver/02_create_rejected_records.sql
+++ sql/02_silver/02_create_rejected_records.sql
@@ -1,1 +1,18 @@
 
+-- ============================================================
+-- CREATE REJECTED RECORDS TABLE
+-- ============================================================
+
+CREATE OR REPLACE TABLE KARTHICKY_DB."2_SILVER".REJECTED_RECORDS (
+
+    REJECT_ID NUMBER AUTOINCREMENT,
+
+    TRANSACTION_ID NUMBER,
+
+    REJECT_REASON VARCHAR,
+
+    RAW_DATA VARIANT,
+
+    _REJECTED_AT TIMESTAMP_NTZ DEFAULT CURRENT_TIMESTAMP()
+
+);
