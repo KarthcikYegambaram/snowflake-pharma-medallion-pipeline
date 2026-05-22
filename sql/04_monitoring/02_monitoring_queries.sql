b24b494c054c6cb65751ab3e3212752c6fdb15d1
Karthick Yegambaram <143865296+KarthcikYegambaram@users.noreply.github.com>
2026-05-22 16:52:04 +0530

    Update 02_monitoring_queries.sql

--- sql/04_monitoring/02_monitoring_queries.sql
+++ sql/04_monitoring/02_monitoring_queries.sql
@@ -1,1 +1,14 @@
 
+-- ============================================================
+-- MONITORING QUERIES
+-- ============================================================
+
+SHOW TASKS IN DATABASE KARTHICKY_DB;
+
+SHOW STREAMS IN DATABASE KARTHICKY_DB;
+
+SELECT *
+FROM TABLE(
+    KARTHICKY_DB.INFORMATION_SCHEMA.TASK_HISTORY()
+)
+ORDER BY SCHEDULED_TIME DESC;
